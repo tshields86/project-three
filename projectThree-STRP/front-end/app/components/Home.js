ab8f426618512d2a2891204ded03f381a3c1b889
sungjin0426 <sjlbest0426@gmail.com>
2016-04-12 16:30:54 -0400

    initial setup

---
+++ projectThree-STRP/front-end/app/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+const Home = React.createClass({
+
+  render: function(){
+    return (
+      <div>
+        <h1>Hello World from STRPers</h1>
+      </div>
+    );
+  }
+});
+
+export default Home;
---
+++ projectThree-STRP/front-end/app/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = React.createClass({
+  render: function(){
+    return(
+      <div>
+        {this.props.children}
+      </div>
+    )
+  }
+});
+
+export default Main;
