499b6d3b41abad46f047ed0df524760d1204ffec
Ryan Othman <ryhoth@gmail>
2016-04-13 14:18:43 -0400

    clrf

--- projectThree-STRP/backend/server/index.js
+++ projectThree-STRP/backend/server/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 
 /* go connect to db first then setup all routes */
-db.connect('mongodb://localhost:27017/possible_name', (err) => {
+db.connect('mongodb://localhost:27017/mapado', (err) => {
   if (err) {
     console.log('Unable to connect to Mongo.')
     process.exit(1)
@@ -23,18 +23,27 @@ db.connect('mongodb://localhost:27017/possible_name', (err) => {
     console.log('connected to mongo');
     /* now start the server and setup all routes! */
 
+    app.push('/')
+
+
     //root route
     app.get('/', (req, res) => {
       res.json({
-        'project three':'api',
+        'mapado':'api',
         'version':'1.0'
       });
     });
+
     // api route
     app.use('/api', api());
     app.server.listen(process.env.PORT || 3000); // use the system port or local
     console.log(`Started on port ${app.server.address().port}`); // template string
   }
 })
+
+/*
+
+
+*/
 
 export default app;
