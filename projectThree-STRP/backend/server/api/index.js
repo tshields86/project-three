26154c22176db0815fd4e9d2ef04c53f813dea49
Travis Shields <travis.shields@gmail.com>
2016-04-13 14:17:53 -0400

    added api get and post

--- projectThree-STRP/backend/server/api/index.js
+++ projectThree-STRP/backend/server/api/index.js
@@ -4,8 +4,23 @@ import { ObjectID } from 'mongodb';
 
 export default function() {
   const api = Router();
-  const collection = db.get().collection('');
+  const collection = db.get().collection('tasks');
 
+  api.get('/tasks', (req, res) => {
+
+    collection.find().toArray((err, docs) => {
+      res.json({ tasks : docs });
+    });
+  });
+
+
+
+  api.post('/task', (req, res) => {
+    // create a new task
+    collection.insert(req.body, (err, result) => {
+      res.json(result);
+    });
+  });
 
   return api;
 }
