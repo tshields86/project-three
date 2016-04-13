50485ade59d4ba584b461b05ed7dfcc6fe2fdc8f
Travis Shields <travis.shields@gmail.com>
2016-04-13 15:03:21 -0400

    added ajax helpers

---
+++ projectThree-STRP/front-end/app/utils/ajaxHelpers.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const helpers = {
+  getTasks: function(){
+    // fetch some data from using a promise
+    return axios.get('http://localhost:3000/api/tasks');
+  },
+  getTask: function(){
+    return axios.get('http://localhost:3000/api/task')
+  },
+  addTask: function(task){
+    return axios.post('http://localhost:3000/api/task', task);
+  },
+  deleteTask: function(task){
+    return axios.delete('http://localhost:3000/api/task', task);
+  },
+  updateTask: function(task){
+    return axios.put('http://localhost:3000/api/task', task);
+  }
+}
+
+export default helpers;
