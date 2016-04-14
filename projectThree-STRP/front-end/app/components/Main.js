0f650b5970979608e66ab700206646391697b015
Ryan Othman <ryhoth@gmail>
2016-04-14 18:55:40 -0400

    load mapbox

--- projectThree-STRP/front-end/app/components/Main.js
+++ projectThree-STRP/front-end/app/components/Main.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import {Link} from 'react-router';
+import mpx from './keys';
 
 const Main = React.createClass({
+  componentDidMount: function(){
+    L.mapbox.accessToken = mpx;
+    var map = L.mapbox.map('map', 'mapbox.streets').setView([40, -74.50], 9);
+  },
   render: function(){
+    const StyleHeader = {
+      position: 'fixed',
+      margin: '1vw',
+      textAlign: "center"
+    }
+    const StyleMap = {
+      width: '95vw',
+      height: '65vw',
+      margin: '0vw',
+      zIndex: '-3000',
+      position: 'fixed'
+    };
+    const StyleRest = {
+      width: '10vw',
+      zIndex: '3000'
+    }
+
     return(
       <div>
-        <h1>Hello World from STRPers</h1>
-        {this.props.children}
+        <h1 style={StyleHeader}>Mapado</h1>
+        <div id='map' style={StyleMap}></div>
+        <div style={StyleRest}>{this.props.children}</div>
       </div>
     )
   }
---
+++ projectThree-STRP/front-end/app/components/keys.js
@@ -0,0 +1,3 @@
+const mpx = 'pk.eyJ1Ijoicnlob3RoIiwiYSI6ImNpbTE3ZTNoajA4dnh0eWtycGNmOTZudzMifQ.Ca9ySwzlSbTZJL3_uOGkAg';
+
+export default mpx
