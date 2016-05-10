98ffa846d3602beb27593ea80deb661ecf953af4
Travis Shields <travis.shields@gmail.com>
2016-05-10 18:55:25 -0400

    adds app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+    app     = express(),
+    PORT    = process.env.PORT || 3000;
+
+app.use(express.static('./dist'));
+
+app.listen(PORT, function(err) {
+  if (err) {
+    console.log('error starting server', err);
+  } else {
+    console.log('server up and running on port', PORT);
+  }
+});
