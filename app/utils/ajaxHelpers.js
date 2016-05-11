1b9443867d7a9576513d8830154db62691adfbd0
Ryan Othman <ryhoth@gmail>
2016-05-11 13:44:39 -0400

    Fix before deployment

--- app/utils/ajaxHelpers.js
+++ app/utils/ajaxHelpers.js
@@ -1,25 +1,25 @@
 import axios from 'axios';
-import kk from '../components/keys.js';
+// import kk from '../components/keys.js';
 
 const helpers = {
   getTasks: function(){
     // fetch some data from using a promise
-    return axios.get('http://localhost:3000/api/tasks');
+    return axios.get('https://mapado-nyc-backend.herokuapp.com/api/tasks');
   },
   getTask: function(task){
-    return axios.get('http://localhost:3000/api/task/'+ task);
+    return axios.get('https://mapado-nyc-backend.herokuapp.com/api/task/'+ task);
   },
   addTask: function(task){
-    return axios.post('http://localhost:3000/api/task', task);
+    return axios.post('https://mapado-nyc-backend.herokuapp.com/api/task', task);
   },
   deleteTask: function(task){
-    return axios.delete('http://localhost:3000/api/task/'+ task);
+    return axios.delete('https://mapado-nyc-backend.herokuapp.com/api/task/'+ task);
   },
   updateTask: function(task){
-    return axios.put('http://localhost:3000/api/task/', task);
+    return axios.put('https://mapado-nyc-backend.herokuapp.com/api/task/', task);
   },
   geoCode: function(address){
-    return axios.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address +',+New+York,+NY&key=' + kk.goog);
+    return axios.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address +',+New+York,+NY&key=' + process.env.GOOG;
   },
 }
 
--- app/utils/mapz.js
+++ app/utils/mapz.js
@@ -1,7 +1,7 @@
 import kk from '../components/keys';
 
 const mapz = {
-  access: L.mapbox.accessToken = kk.mpx,
+  access: L.mapbox.accessToken = process.env.MPX,
   map: L.mapbox.map('map', 'mapbox.streets').setView([40.7527, -73.9772], 13)
 }
 
